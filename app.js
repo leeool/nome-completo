f6b915369b7c07614ef9949eadc8a8b90026116a
leeool <lgonsalez02@gmail.com>
2022-10-29 16:52:40 -0300

    finaliza exercicio

---
+++ app.js
@@ -0,0 +1,5 @@
+const firstName = prompt("Insira o seu primeiro nome:")
+const lastName = prompt("Insira o seu sobrenome:")
+
+alert(`Nome completo: ${firstName} ${lastName}
+Nome catálogo: ${lastName.toUpperCase()}, ${firstName}`)
